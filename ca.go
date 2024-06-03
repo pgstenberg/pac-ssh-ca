767837c25cff9a524919d37611266511d9354474
Per-Gustaf Stenberg <pegge.stenberg@gmail.com>
2024-06-03 12:00:44 +0200

    Removed redundant import in ca.go

--- ca.go
+++ ca.go
@@ -4,25 +4,24 @@ import (
 	"crypto/rsa"
 	"fmt"
 
-	"golang.org/x/crypto/ssh"
 	cryptossh "golang.org/x/crypto/ssh"
 )
 
 type certificateAuthority struct {
-	signer     *ssh.Signer
+	signer     *cryptossh.Signer
 	publicKey  *rsa.PublicKey
 	privateKey *rsa.PrivateKey
 
-	delegates []*ssh.PublicKey
+	delegates []*cryptossh.PublicKey
 }
 
-func loadDelegates(delegates [][]byte) ([]*ssh.PublicKey, error) {
-	list := []*ssh.PublicKey{}
+func loadDelegates(delegates [][]byte) ([]*cryptossh.PublicKey, error) {
+	list := []*cryptossh.PublicKey{}
 	for _, issuerData := range delegates {
 		if len(issuerData) == 0 {
 			continue
 		}
-		k, _, _, _, err := ssh.ParseAuthorizedKey(issuerData)
+		k, _, _, _, err := cryptossh.ParseAuthorizedKey(issuerData)
 		if err != nil {
 			return nil, err
 		}
@@ -41,12 +40,12 @@ func (ca *certificateAuthority) delegateFingerprints() []string {
 }
 
 func newCertificateAuthority(privatekey []byte, delegates [][]byte) (*certificateAuthority, error) {
-	pk, err := ssh.ParseRawPrivateKey(privatekey)
+	pk, err := cryptossh.ParseRawPrivateKey(privatekey)
 	if err != nil {
 		return nil, err
 	}
 
-	signer, err := ssh.NewSignerFromKey(pk)
+	signer, err := cryptossh.NewSignerFromKey(pk)
 	if err != nil {
 		return nil, err
 	}
@@ -57,7 +56,7 @@ func newCertificateAuthority(privatekey []byte, delegates [][]byte) (*certificat
 		privateKey: pk.(*rsa.PrivateKey),
 	}
 
-	ca.delegates = []*ssh.PublicKey{}
+	ca.delegates = []*cryptossh.PublicKey{}
 
 	if len(delegates) > 0 {
 		ca.delegates, err = loadDelegates(delegates)
